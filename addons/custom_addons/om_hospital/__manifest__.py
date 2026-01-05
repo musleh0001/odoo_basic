cb7cf1980cf1c234d555b688b606df186d08bdc0
musleh-khan <MuslehUddin.khan@exos.systems>
2026-01-05 15:25:44 +0600

    feat: adds compute fields

--- addons/custom_addons/om_hospital/__manifest__.py
+++ addons/custom_addons/om_hospital/__manifest__.py
@@ -4,15 +4,21 @@
     "license": "LGPL-3",
     "version": "17.0.1.0",
     "depends": [
-        "mail"
+        "mail",
+        "product",
     ],
     "data": [
         "security/ir.model.access.csv",
+
         "data/sequence.xml",
+
         "views/patient_views.xml",
         "views/patient_tag_views.xml",
         "views/patient_readonly_views.xml",
+
         "views/appointment_views.xml",
+        "views/appointment_line_views.xml",
+
         "views/menu.xml",
     ],
 }
--- addons/custom_addons/om_hospital/models/appointment.py
+++ addons/custom_addons/om_hospital/models/appointment.py
@@ -5,6 +5,7 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ["mail.thread"]
     _description = "Hospital Appointment"
+    _rec_names_search = ["reference", "patient_id"]
     _rec_name = "patient_id"
 
     reference = fields.Char(string="reference", default="New")
@@ -22,6 +23,8 @@ class HospitalAppointment(models.Model):
         default="draft",
         tracking=True
     )
+    appointment_line_ids = fields.One2many("hospital.appointment.line", "appointment_id", string="Lines") 
+    total_qty = fields.Float(compute="_compute_total_qty", string="Total Quantity")
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -29,6 +32,14 @@ class HospitalAppointment(models.Model):
             if vals.get("reference", "New") == "New":
                 vals["reference"] = self.env["ir.sequence"].next_by_code("hospital.appointment")
         return super().create(vals_list)
+
+    def _compute_total_qty(self):
+        for rec in self:
+            rec.total_qty = sum(rec.appointment_line_ids.mapped("qty"))
+
+    def _compute_display_name(self):
+        for rec in self:
+            rec.display_name = f"[{rec.reference}] {rec.patient_id.name}"
 
     def action_confirm(self):
         for rec in self:
@@ -46,3 +57,11 @@ class HospitalAppointment(models.Model):
         for rec in self:
             rec.state = "cancel"
 
+
+class HospitalAppointmentLine(models.Model):
+    _name = "hospital.appointment.line"
+    _description = "Hospital Appointment Line"
+
+    appointment_id = fields.Many2one("hospital.appointment", string="Appointment")
+    product_id = fields.Many2one("product.product", string="Product", required=True)
+    qty = fields.Float(string="Quantity")
