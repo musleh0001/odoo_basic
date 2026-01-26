30e4dc48a1d7096c4e2f6bf3ae9b62c9025daced
musleh-khan <MuslehUddin.khan@exos.systems>
2026-01-26 18:45:56 +0600

    feat: security

--- addons/custom_addons/om_hospital/__manifest__.py
+++ addons/custom_addons/om_hospital/__manifest__.py
@@ -10,6 +10,7 @@
     ],
     "data": [
         "security/ir.model.access.csv",
+        "security/security.xml",
         "data/sequence.xml",
         "views/patient_views.xml",
         "views/patient_readonly_views.xml",
